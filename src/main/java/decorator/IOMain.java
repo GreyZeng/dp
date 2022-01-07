c6f03f1c51cb6ca34a88b40922b6abcd12702ef5
zenghui <410486047@qq.com>
2022-01-07 15:58:13 +0800

    decorator pattern

--- src/main/java/decorator/IOMain.java
+++ src/main/java/decorator/IOMain.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 // 装饰器的在jdk中的一个应用
 public class IOMain {
     public static void main(String[] args) throws Exception {
-     
+
         File f = new File("D:/1.txt");
         FileOutputStream fos = new FileOutputStream(f);
         OutputStreamWriter osw = new OutputStreamWriter(fos);
