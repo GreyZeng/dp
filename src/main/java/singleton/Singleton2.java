f2b90a1485f8114d1b5757d978c1112e38a9e075
zenghui <410486047@qq.com>
2022-01-07 15:07:09 +0800

    lazy mode

--- src/main/java/singleton/Singleton2.java
+++ src/main/java/singleton/Singleton2.java
@@ -14,7 +14,9 @@ public class Singleton2 {
     static {
         INSTANCE = new Singleton2();
     }
-
+    private Singleton2() {
+    	
+    }
     public static Singleton2 getInstance() {
         return INSTANCE;
     }
