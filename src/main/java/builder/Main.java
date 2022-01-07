92e5391e95cb1d44e8fd10611a8839743c624b55
zenghui <410486047@qq.com>
2022-01-07 15:24:13 +0800

    update code

--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -4,16 +4,13 @@ import static builder.NyPizza.Size.SMALL;
 import static builder.Pizza.Topping.*;
 
 public class Main {
-    public static void main(String[] args) {
-        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
-                .calories(100).sodium(35).carbohydrate(27).build();
+	public static void main(String[] args) {
+		NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
 
-        System.out.println(cocaCola);
-        NyPizza pizza = new NyPizza.Builder(SMALL)
-                .addTopping(SAUSAGE).addTopping(ONION).build();
-        System.out.println(pizza);
-        Calzone calzone = new Calzone.Builder()
-                .addTopping(HAM).sauceInside().build();
-        System.out.println(calzone);
-    }
+		System.out.println(cocaCola);
+		NyPizza pizza = new NyPizza.Builder(SMALL).addTopping(SAUSAGE).addTopping(ONION).build();
+		System.out.println(pizza);
+		Calzone calzone = new Calzone.Builder().addTopping(HAM).sauceInside().build();
+		System.out.println(calzone);
+	}
 }
