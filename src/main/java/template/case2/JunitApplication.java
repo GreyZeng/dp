57fd8a0025c71117fed470145d8ff3d0aa53bc66
GreyZeng <410486047@qq.com>
2022-11-11 17:01:45 +0800

    update notes

--- src/main/java/template/case2/JunitApplication.java
+++ src/main/java/template/case2/JunitApplication.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JunitApplication {
-	private static final List<TestCase> testCases = new ArrayList<>();
+    private static final List<TestCase> testCases = new ArrayList<>();
 
-	public static void register(TestCase testCase) {
-		testCases.add(testCase);
-	}
+    public static void register(TestCase testCase) {
+        testCases.add(testCase);
+    }
 
-	public static final void main(String[] args) {
-		for (TestCase c : testCases) {
-			c.run();
-		}
-	}
+    public static final void main(String[] args) {
+        for (TestCase c : testCases) {
+            c.run();
+        }
+    }
 }
--- src/main/java/template/case2/UserServiceTest.java
+++ src/main/java/template/case2/UserServiceTest.java
@@ -2,10 +2,10 @@ package template.case2;
 
 public class UserServiceTest extends TestCase {
 
-	@Override
-	public boolean doTest() {
-		System.out.println("do test...");
-		return false;
-	}
+    @Override
+    public boolean doTest() {
+        System.out.println("do test...");
+        return false;
+    }
 
 }
