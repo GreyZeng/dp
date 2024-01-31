488009f894064ac04984452e1fa676b61aa86b94
GreyZeng <410486047@qq.com>
2024-01-31 12:41:47 +0800

    update code

--- src/main/java/prototype/Main.java
+++ src/main/java/prototype/Main.java
@@ -8,20 +8,30 @@ import java.util.List;
  * @date 2020/4/17
  */
 public class Main {
+
     public static void main(String[] args) throws CloneNotSupportedException {
+        shallowClone();
+        deepClone();
+    }
+
+    // 浅克隆(基本类型是复制，对象类型只拷贝引用）
+    public static void shallowClone() throws CloneNotSupportedException {
+        System.out.println("浅克隆");
         O o = new O();
-        System.out.println(o.clone() == o); // false
-        System.out.println(o.clone().equals(o)); // false
+        O clonedO = (O)o.clone();
+        System.out.println(clonedO == o); // false
+        System.out.println(clonedO.equals(o)); // false
+        System.out.println(o.obj1 == clonedO.obj1); // true
         System.out.println();
         Person p = new Person();
         System.out.println(p);
         Person p2 = (Person) p.clone();
         System.out.println(p2);
-        System.out.println("使用序列化方式进行深度克隆");
-        deepClone();
     }
+
     // 使用序列化的方式进行深克隆
     public static void deepClone() {
+        System.out.println("使用序列化方式进行深度克隆");
         List<String> hobbies = new ArrayList<>();
         hobbies.add("唱歌");
         hobbies.add("跳舞");
@@ -37,8 +47,16 @@ public class Main {
 }
 
 class O implements Cloneable {
+
+    public int num;
+    public MyObject obj1;
+
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+}
+
+class MyObject {
+
 }
--- src/main/java/prototype/Prototype.java
+++ src/main/java/prototype/Prototype.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
