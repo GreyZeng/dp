dc77a6de979d440cbce61519df478ea173e8477c
GreyZeng <410486047@qq.com>
2024-01-31 11:23:14 +0800

    update code

--- src/main/java/factory/factorymethod/Main.java
+++ src/main/java/factory/factorymethod/Main.java
@@ -1,10 +1,22 @@
 package factory.factorymethod;
 
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         new HpMouseFactory().createMouse().sayHi();
         new DellMouseFactory().createMouse().sayHi();
         new IBMMouseFactory().createMouse().sayHi();
+
+        MovableFactory.create(Car::new).go();
+        MovableFactory.create(Ship::new).go();
+    }
+
+    static class MovableFactory {
+        // 使用 Supplier 实现单例模式
+        public static Movable create(Supplier<? extends Movable> supplier) {
+            return supplier.get();
+        }
     }
 }
