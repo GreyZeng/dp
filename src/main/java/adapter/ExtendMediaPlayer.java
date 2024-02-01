82b9582e9ee1a9ca9b8115043cb51546f0db5eb7
GreyZeng <410486047@qq.com>
2024-02-01 09:38:30 +0800

    update

--- src/main/java/adapter/ExtendMediaPlayer.java
+++ src/main/java/adapter/ExtendMediaPlayer.java
@@ -5,11 +5,9 @@ package adapter;
  * @date 2020/4/16
  */
 public class ExtendMediaPlayer implements MediaPlayer {
-    private PlayerAdapter adapter;
-
     @Override
     public void play(String type, String fileName) {
-        adapter = new PlayerAdapter(type);
+        PlayerAdapter adapter = new PlayerAdapter(type);
         adapter.play(type, fileName);
     }
 }
