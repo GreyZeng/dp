0c45d1c1acba8f7696d4ace2fbfd5ca55d4ad87a
GreyZeng <410486047@qq.com>
2024-02-01 23:01:21 +0800

    update code

--- src/main/java/decorator/IOMain.java
+++ src/main/java/decorator/IOMain.java
@@ -1,20 +1,21 @@
 package decorator;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
-// 装饰器的在jdk中的一个应用
+// 装饰器模式在jdk中的一个应用
 public class IOMain {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
         File f = new File("D:/1.txt");
-        FileOutputStream fos = new FileOutputStream(f);
-        OutputStreamWriter osw = new OutputStreamWriter(fos);
-        BufferedWriter bw = new BufferedWriter(osw);
-        bw.write("abc");
-        bw.flush();
-        bw.close();
+        try (
+                FileOutputStream fos = new FileOutputStream(f);
+                OutputStreamWriter osw = new OutputStreamWriter(fos);
+                // BufferedWriter用来装饰OutputStreamWriter
+                BufferedWriter bw = new BufferedWriter(osw);
+        ) {
+            bw.write("abc");
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
