3ac1158e65abbe9e09f18a0f083dfb26016b57d4
GreyZeng <410486047@qq.com>
2024-02-02 23:49:29 +0800

    update code

--- src/main/java/Iterator/Main.java
+++ src/main/java/Iterator/Main.java
@@ -1,10 +1,28 @@
 package Iterator;
+
 /**
  * @author Grey
  * @date 2020/4/15
  */
 public class Main {
     public static void main(String[] args) {
+        testIterator();
+        testIterator2();
+    }
+
+    private static void testIterator2() {
+        ArrayList_<Obj> list = new ArrayList_<>();
+        for (int i = 0; i < 12; i++) {
+            list.add(new Obj(i));
+        }
+        System.out.println(list.size());
+        Iterator_<Obj> iterator_ = list.iterator();
+        while (iterator_.hasNext()) {
+            System.out.println(iterator_.next());
+        }
+    }
+
+    private static void testIterator() {
         ArrayList_<String> list = new ArrayList_<>();
         for (int i = 0; i < 12; i++) {
             list.add("str" + i);
@@ -14,5 +32,20 @@ public class Main {
         while (iterator_.hasNext()) {
             System.out.println(iterator_.next());
         }
+    }
+}
+
+class Obj {
+    private final int num;
+
+    public Obj(int n) {
+        num = n;
+    }
+
+    @Override
+    public String toString() {
+        return "Obj{" +
+                "num=" + num +
+                '}';
     }
 }
