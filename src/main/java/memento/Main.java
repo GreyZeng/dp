64d021eacecdecb26a1caa9693fde1651d321d46
GreyZeng <410486047@qq.com>
2024-02-02 23:33:59 +0800

    update code

--- src/main/java/memento/Main.java
+++ src/main/java/memento/Main.java
@@ -1,13 +1,14 @@
 package memento;
 
 import java.io.*;
+import java.nio.file.Files;
 
 /**
  * @author Grey
  * @date 2020/4/17
  */
 public class Main {
-
+    private final static String FILE_PATH= "C:\\workspace\\tank.data";
     public static void main(String[] args) {
         Person person = new Person();
         person.name = "zhangsan";
@@ -17,8 +18,8 @@ public class Main {
     }
 
     public void save(Person person) {
-        File c = new File("/tank.data");
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(c));) {
+        File c = new File(FILE_PATH);
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(c.toPath()));) {
             oos.writeObject(person);
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,14 +27,12 @@ public class Main {
     }
 
     public void load() {
-        File c = new File("/tank.data");
-        try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream(c));) {
+        File c = new File(FILE_PATH);
+        try (ObjectInputStream oos = new ObjectInputStream(Files.newInputStream(c.toPath()));) {
 
             Person myTank = (Person) oos.readObject();
             System.out.println(myTank);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
